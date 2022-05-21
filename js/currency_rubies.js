0572863e0695492be369a27e0bee7197cf696893
Enrique Jimenez <enriquejizquierdo@gmail.com>
2022-05-21 21:00:17 +0200

    Hide rubies display before aquiring them

--- js/currency_rubies.js
+++ js/currency_rubies.js
@@ -4,5 +4,6 @@ var rubies = 0;
 
 function UpdateRubiesUI()
 {
+    $('#rubies-count').css('display', rubies == 0 ? 'none' : 'block');
     $('#rubies-count').text('Rubies = '+ FormatNumber(rubies));
 }
